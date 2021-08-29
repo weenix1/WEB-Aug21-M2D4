438de14e58acee2ba8d562b9de6cb4e6b3e503cc
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-08-29 15:34:09 +0200

    new commit

--- app.js
+++ app.js
@@ -1,48 +1,23 @@
 /* Features
                     Create an input to get the names to add to the list ("waiting list").
                     Create an input to get the number of teams the user wants to generate.
+
                     Create the grid for displaying the teams. Each team should be rendered inside of a column.
                     Make sure the previously specified number of teams gets generated before proceding with the next step.
                     Create an “Assign” button near the “waiting list”. Every time the user clicks on it, a randomly picked name is placed in a team and removed from the list of people.
                  */
-
-/*  Ex1.Create an input to get the names to add to the list ("waiting list"). */
+const inputName = document.querySelector("#input-name");
+const teamQueue = document.querySelector("#team-queue");
 const teamsN = document.querySelector("#teams-number");
 const teamsRow = document.querySelector("#teams-row");
-const teamQueue = document.querySelector("#team-queue");
 
-function addNameToTeam() {
-  let text = document.getElementById("team-name").value;
-  let li = document.createElement("li");
-  let newList = document.querySelector(".team-list");
-  li.classList.add("team-list");
-  li.innerHTML = text;
-  newList.appendChild(li);
-}
-console.log(addNameToTeam());
-/* 
-function createTeam() {
-  let numOfTeam = document.getElementById("number");
-  const teamMem = document.getElementById("team-name");
-  const newTeam = teamMem.value;
-
-  for (let i = 0; i <= numOfTeam; i++) {
-    let newTeamList = document.createElement("li");
-    newTeamList.classList.add("team-list");
-    newTeamList.innerHTML = newTeam;
-  }
-} */
-
-function createTeams() {
-  for (let i = 0; i < teamsN.innerHTML; i++) {
-    teamsRow.innerHTML += `
-          <div class="col-6 col-sm-3 team">
-              <h4>Team ${i + 1}</h4>
-              <ul class="list-group">
-              </ul>
-          </div>
-      `;
-  }
+function addPerson(event) {
+  event.preventDefault();
+  console.log(inputName.value);
+  const li = document.createElement("li");
+  li.classList.add("list-group-item");
+  li.innerHTML = inputName.value;
+  teamQueue.appendChild(li);
 }
 
 window.onload = function () {
@@ -50,18 +25,32 @@ window.onload = function () {
   createTeams();
 };
 
+function createTeams() {
+  for (let i = 0; i < teamsN.innerHTML; i++) {
+    teamsRow.innerHTML += `
+      <div class="col-6 col-sm-3 team">
+        <h4>Team ${i + 1}</h4>
+        <ul class="list-group"></ul>
+      </div> `;
+  }
+}
+
 function addTeam() {
   const teamNumber = parseInt(teamsN.innerHTML) + 1;
-
   teamsN.innerHTML = teamNumber;
-
   teamsRow.innerHTML += `
       <div class="col-6 col-sm-3 team">
-          <h4>Team ${teamNumber}</h4>
-          <ul class="list-group">
-          </ul>
-      </div>
-  `;
+        <h4>Team ${teamNumber}</h4>
+        <ul class="list-group"></ul>
+      </div> `;
+}
+
+function assign() {
+  const person = teamQueue.children[0];
+  if (!person) return;
+  const teams = document.querySelectorAll(".team > ul");
+  const randomIndex = Math.floor(Math.random() * teams.length);
+  teams[randomIndex].appendChild(person);
 }
 
 function removeTeam() {
@@ -83,19 +72,6 @@ function removeTeam() {
   lastTeam.remove();
 }
 
-function assign() {
-  const person = teamQueue.children[0];
-
-  if (!person) return;
-
-  const teams = document.querySelectorAll(".team > ul");
-  const randomIndex = Math.floor(Math.random() * teams.length);
-
-  teams[randomIndex].appendChild(person);
-
-  console.log({ person, randomIndex });
-}
-
 function reset() {
   teamQueue.innerHTML = "";
 
@@ -107,30 +83,3 @@ function reset() {
 
   createTeams();
 }
-
-/* const waitingList = function (str) {
-  let list = [];
-  list.push(JSON.parse(JSON.stringify(str)));
-  console.log(list);
-};
- */
-/* ex2.  Create an input to get the number of teams the user wants to generate. */
-/* function createRandomTeamMem(eventData) {
-  const teamMem = document.querySelector(".random-num");
-  let newList = document.querySelectorAll("ul");
-  let newTeamList;
-  for (let number of teamMem) {
-    newTeamList += Math.round(Math.random() * number);
-  }
-} */
-
-/*
-                        let waitList = document.querySelector("#modal")
-                        if(waitList===waitList){
-                          console.log(waitList + "already exist")
-                        }else{
-                          return waitList
-                        }
-                        for
-      };
-            */
